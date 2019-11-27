f4e40de1e404794791069f18b783b0dae8e2f794
Oumeima IBNELFEKIH <oumeima.ibnelfekih@esprit.tn>
2019-11-27 20:33:23 +0100

    CandidateService_InitialCommit

---
+++ src/app/models/Candidate.ts
@@ -0,0 +1,20 @@
+export class Candidate {
+    id: number;
+    first_Name: string;
+    last_Name: string;
+    email: string;
+    password: string ;
+    active: boolean ;
+    confirmationToken: string ;
+    studyLevel: string ;
+    profilIntro: string ;
+    activities: string ;
+    phoneNumber: number ;
+    Certifications: string ;
+    experiences: string ;
+    age: string ;
+    post: string ;
+
+
+
+}
---
+++ src/app/services/candidate.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
+import {Candidate} from '../models/Candidate';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidateService {
+  apiURL = '/api/Candidate';
+  apiURLProfil = '/api/Candidate/profileCandidate';
+  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+
+
+    })
+  };
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+      console.error('An error occurred:', error.message);
+      console.error('An error occurred:', error.headers);
+      console.error('An error occurred:', error.url);
+      console.log('ERRRR ENDS HERE CLIENT SIDED');
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`,
+          `MSG was: ${error.headers}`,
+          `Error is :${error.message}`);
+      throwError(error);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+        error);
+  }
+  createCandidate(candidate): Observable<Candidate> {
+
+    return this.http.post<Candidate>(this.apiURL, JSON.stringify(candidate),  this.httpOptions
+    ).pipe(
+        catchError(this.handleError)
+
+    );
+  }
+
+
+  getCandidate(): Observable<Candidate> {
+    return this.http.get<Candidate>(this.apiURLProfil, this.httpOptions )
+        .pipe(
+            retry(1),
+            catchError(this.handleError)
+        );
+  }
+}
